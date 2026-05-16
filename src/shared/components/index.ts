7de11dbf705bb2d4aee572153f3323e87e2c0180
astrocoding <zaenalalfian20@gmail.com>
2026-05-16 09:54:01 +0700

    feats(shared): added map file & directory shared components

---
+++ src/shared/components/index.ts
@@ -0,0 +1,34 @@
+// export * from "./atoms/Button";
+// export * from "./atoms/IconButton";
+// export * from "./atoms/Input";
+// export * from "./atoms/Textarea";
+// export * from "./atoms/Select";
+// export * from "./atoms/Checkbox";
+// export * from "./atoms/Radio";
+// export * from "./atoms/Badge";
+// export * from "./atoms/Avatar";
+// export * from "./atoms/Spinner";
+// export * from "./atoms/Divider";
+// export * from "./atoms/Typography";
+
+// export * from "./molecules/FormField";
+// export * from "./molecules/SearchInput";
+// export * from "./molecules/Alert";
+// export * from "./molecules/Card";
+// export * from "./molecules/Breadcrumb";
+// export * from "./molecules/Pagination";
+// export * from "./molecules/Tabs";
+// export * from "./molecules/EmptyState";
+// export * from "./molecules/RichText";
+
+// export * from "./organisms/Header";
+// export * from "./organisms/Sidebar";
+// export * from "./organisms/Footer";
+// export * from "./organisms/Modal";
+// export * from "./organisms/Drawer";
+// export * from "./organisms/DataTable";
+// export * from "./organisms/Navbar";
+
+// export * from "./templates/PublicLayoutShell";
+// export * from "./templates/DashboardLayoutShell";
+// export * from "./templates/TwoColumnLayout";
